bc901afecb3a063058f6dd1eb33726537a46da2c
Malgorzata <mlesniak.priv@gmail.com>
2021-08-31 02:39:48 +0200

    added postgres to horeoku

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY = 'stable'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://owner:owner@localhost/stable'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://owner:owner@localhost/stable')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
