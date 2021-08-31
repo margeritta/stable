6ffecc65aaa9b2854f1d08e0f666c8a2f65e6646
Malgorzata <mlesniak.priv@gmail.com>
2021-08-31 02:50:46 +0200

    fixed postgres connection

--- app/config.py
+++ app/config.py
@@ -1,6 +1,10 @@
 import os
+import re
 
 class Config(object):
+    uri = os.getenv("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
     SECRET_KEY = 'stable'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://owner:owner@localhost/stable')
+    SQLALCHEMY_DATABASE_URI = os.environ.get(uri, 'postgresql://owner:owner@localhost/stable')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
